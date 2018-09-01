ec226e8631f93859711320576259d4428ecb70a9
Charvi bansal <32154571+charvi1234@users.noreply.github.com>
2018-09-01 20:27:37 +0530

    Add files via upload

---
+++ balanced_parenthesis.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<stack>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str[1000]="{()[()]}{}([]){}";
+    int len=strlen(str);
+    stack <char> s;
+    s.push(str[0]);
+    int i=1,j;
+    while(str[i]!='\0')
+    {
+        if(str[i]=='{'||str[i]=='('||str[i]=='[')
+        {
+            s.push(str[i]);
+            i++;
+        }
+        else
+        {
+            if(str[i]=='}')
+            {
+                if(s.empty())
+                    break;
+                if(s.top()=='{')
+                {
+                    s.pop();
+                }
+
+            }
+           else if(str[i]==']')
+            {
+                if(s.empty())
+                    break;
+                if(s.top()=='[')
+                {
+                    s.pop();
+                }
+                else
+                    break;
+            }
+            else if(str[i]==')')
+            {
+                if(s.empty())
+                    break;
+                if(s.top()=='(')
+                {
+                    s.pop();
+                }
+                else
+                    break;
+            }
+            i++;
+        }
+    }
+    if(s.empty()&&str[i]=='\0')
+        cout<<"valid parenthesis";
+    else
+        cout<<"invalid parenthesis";
+}
